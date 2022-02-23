5824288920dc0086b17107dd83a595ef6b45dfd7
Levani1111 <papashvililevani@gmail.com>
2022-02-23 12:29:08 -0500

    active class with js

--- app.js
+++ app.js
@@ -13,6 +13,23 @@ function PageTransitions() {
               this.className += ' active-btn';
         }) 
     }
+    // Sections active class
+    allSections.addEventListener('click', function(e) {
+        const id = e.target.dataset.id;
+        if(id) {
+            // remove deleted from other buttons
+            sectBtn.forEach((btn) => {
+                btn.classList.remove('active');
+            })
+            e.target.classList.add('active');
+            // hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active');
+            })
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 PageTransitions() // call function
